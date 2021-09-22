cb1bc381fd1d64c9b3f484ef60aa5619134ddd4b
maxradchenko20 <maxradchenko20@gmail.com>
2021-09-22 18:01:49 +0300

    Create the first component

---
+++ src/components/Landing/landing.js
@@ -0,0 +1,31 @@
+import React from "react";
+import restaurants from "../../sample-restaurants";
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className="restaurant_select">
+        <div className="restaurant_select_top">
+          <div className="restaurant_select_top-header">Выбери ресторан</div>
+
+          <div className="arrow_picker">
+            <div className="arrow_picker-up"></div>
+            <div className="arrow_picker-down"></div>
+          </div>
+        </div>
+
+        <div className="restaurant_select_bottom">
+          <ul>
+            {
+              restaurants.map(res => {
+                return <li key={res.id}>{res.title}</li>
+              })}
+          </ul>
+        </div>
+        <button>Перейти в ресторан</button>
+      </div>
+    );
+  }
+}
+
+export default Landing
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {render} from "react-dom";
+import Landing from "./components/Landing/landing";
+
+
+render(<Landing/>, document.querySelector('#root'));
