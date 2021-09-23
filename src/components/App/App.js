a52c7043908e233317c0e2045a42316c186ae2ae
maxradchenko20 <maxradchenko20@gmail.com>
2021-09-23 21:12:20 +0300

    Form and object creation state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,20 @@ import MenuAdmin from "../MenuAdmin/MenuAdmin";
 
 
 class App extends React.Component {
+  state = {
+    burgers: {},
+    order: {}
+  }
+
+   addBurger = (burger) => {
+     //Делаем копию обьекта state
+     const burgers = {...this.state.burgers};
+     //Добавить новый бургер в переменную burgers
+     burgers[`burger${Date.now()}`] = burger;
+     //Записать наш новый обьект burgers в state
+     this.setState({burgers})
+  }
+
   render() {
     return (
       <div className="burger-paradise">
@@ -12,7 +26,7 @@ class App extends React.Component {
           <Header title="Very Hot Burger"/>
         </div>
         <Order/>
-        <MenuAdmin/>
+        <MenuAdmin addBurger={this.addBurger}/>
       </div>
     );
   }
